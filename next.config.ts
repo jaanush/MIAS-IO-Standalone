ac8ab8ae8e958db667f1e77869d681b2ab7afe0c
Jaanus Heeringson <jaanus.heeringson@metstech.se>
2026-03-18 09:38:27 +0100

    deploy: 2026-03-18 09:38:27

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "standalone",
-  serverExternalPackages: ["@prisma/adapter-pg"],
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
